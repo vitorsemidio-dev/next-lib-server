5f2e83a2dac4039af451b39e70d09e16dd2542c2
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-17 14:35:34 -0300

    Criação UsersRepository

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,10 @@
+/** @format */
+
+import { EntityRepository, Repository } from 'typeorm';
+
+import User from '../database/entities/User';
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {}
+
+export default UsersRepository;
