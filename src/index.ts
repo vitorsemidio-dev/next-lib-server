f394cf9d974c96157c8077e7846ab19b03adc5d4
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-17 14:37:28 -0300

    Atualização index.ts e adição handle server error

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 /** @format */
 
+import 'express-async-errors';
+import 'reflect-metadata';
+
 import './database';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import routers from './routers';
 
 const app = express();
+app.use(express.json());
+app.use(routers);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+	console.error(err);
+
+	return response.status(500).json({
+		status: 'error',
+		message: 'Internal server error',
+	});
+});
 
 export default app;
