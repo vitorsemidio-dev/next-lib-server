6dcc8a54a9a57072110470061efd7d6674f94fad
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-18 01:15:53 -0300

    Adição métodos findByEmail e findBySlug ao LibrariesRepository

--- src/repositories/LibrariesRepository.ts
+++ src/repositories/LibrariesRepository.ts
@@ -5,6 +5,22 @@ import { Repository, EntityRepository } from 'typeorm';
 import Library from '../database/entities/Library';
 
 @EntityRepository(Library)
-class LibrariesRepository extends Repository<Library> {}
+class LibrariesRepository extends Repository<Library> {
+	public async findByEmail(email: string): Promise<Library | undefined> {
+		const library = await this.findOne({
+			where: { email },
+		});
+
+		return library;
+	}
+
+	public async findBySlug(slug: string): Promise<Library | undefined> {
+		const library = await this.findOne({
+			where: { slug },
+		});
+
+		return library;
+	}
+}
 
 export default LibrariesRepository;
