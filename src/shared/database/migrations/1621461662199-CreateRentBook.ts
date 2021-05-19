50bc02504f24f9a5d519a1a83e501926beaf743c
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-05-19 20:53:49 -0300

    Criação tabela Rent Books

---
+++ src/shared/database/migrations/1621461662199-CreateRentBook.ts
@@ -0,0 +1,33 @@
+/** @format */
+
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateRentBook1621461662199 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: 'rent_books',
+				columns: [
+					{
+						name: 'id',
+						type: 'varchar',
+					},
+					{
+						name: 'created_at',
+						type: 'timestamp',
+						default: 'now()',
+					},
+					{
+						name: 'updated_at',
+						type: 'timestamp',
+						default: 'now()',
+					},
+				],
+			}),
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('rent_books');
+	}
+}
