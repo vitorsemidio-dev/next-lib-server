2eabdb15b8211f55682371a28300de4eae5ab475
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 03:51:46 -0300

    Correçao autenticacao usuario

--- src/modules/users/controllers/SessionsController.ts
+++ src/modules/users/controllers/SessionsController.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
+import UsersRepository from '../repositories/UsersRepository';
 import AuthService from '../services/AuthService';
-
-let authService: AuthService;
 
 export default class SessionsController {
 	public async create(request: Request, response: Response): Promise<Response> {
 		const { email, password } = request.body;
 
-		authService = new AuthService();
+		const authService = container.resolve(AuthService);
 
 		const { user, token } = await authService.execute({
 			email,
