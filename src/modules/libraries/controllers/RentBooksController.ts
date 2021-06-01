3433288fcd19e36d38b5c1438adbf793f318c459
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 17:57:00 -0300

    Remoção codigo não utilizado

--- src/modules/libraries/controllers/RentBooksController.ts
+++ src/modules/libraries/controllers/RentBooksController.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-import { getRepository } from 'typeorm';
 
 import StockLibraryRepository from '@modules/libraries/repositories/StockLibraryRepository';
 import UsersRepository from '@modules/users/repositories/UsersRepository';
-import RentBook from '@shared/database/entities/RentBook';
 
 import BooksRepository from '../repositories/BooksRepository';
 
@@ -13,13 +11,13 @@ import RentBooksRepository from '../repositories/RentBooksRepository';
 
 export default class RentBooksController {
 	public async create(request: Request, response: Response) {
-		const { user_id, library_id, book_id } = request.body;
+		const { user_id, book_id } = request.body;
 
 		const rentBookRepository = container.resolve(RentBooksRepository);
-
 		const stockLibraryRepository = container.resolve(StockLibraryRepository);
 		const usersRepository = container.resolve(UsersRepository);
 		const booksRepository = container.resolve(BooksRepository);
+
 		const rentBookService = new UserRentBookService(
 			usersRepository,
 			booksRepository,
