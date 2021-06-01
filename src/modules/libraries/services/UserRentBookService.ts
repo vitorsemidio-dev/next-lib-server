e176b3ab14e69b2fe53fff134c6deb19a5ad97ab
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 17:24:36 -0300

    Remoção logs... E adição throw erros

--- src/modules/libraries/services/UserRentBookService.ts
+++ src/modules/libraries/services/UserRentBookService.ts
@@ -25,30 +25,21 @@ export default class UserRentBookService {
 	) {}
 
 	public async execute({ user_id, book_id }: IRequest): Promise<any> {
-		console.log('UserRentBookService');
-		console.log(`User Id: [${user_id}] | Book Id: [${book_id}]`);
-
 		const [userExists, bookExists] = await Promise.all([
 			this.usersRepository.findById(user_id),
 			this.booksRepository.findByIdWithRelations(book_id),
 		]);
 
 		if (!userExists) {
-			// throw new AppError('User does not exists', 404)
-			console.log('[ERROR 404]: User does not exists');
-			return;
+			throw new AppError('User does not exists', 404);
 		}
 
 		if (!bookExists) {
-			// throw new AppError('Book does not exists', 404)
-			console.log('[ERROR 404]: Book does not exists');
-			return;
+			throw new AppError('Book does not exists', 404);
 		}
 
 		if (bookExists.stockLibrary.length === 0) {
-			// throw new AppError('Without book in stock', 400)
-			console.log('[ERROR 400]: Without book in stock');
-			return;
+			throw new AppError('Without book in stock', 400);
 		}
 
 		const stockItem = bookExists.stockLibrary[0];
