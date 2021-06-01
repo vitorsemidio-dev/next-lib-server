345c9544b08c574a849fff156240c15c8e091217
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 16:18:57 -0300

    Validação se usuário existe

--- src/modules/libraries/services/UserRentBookService.ts
+++ src/modules/libraries/services/UserRentBookService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import UsersRepository from '@modules/users/repositories/UsersRepository';
+import AppError from '@shared/errors/AppError';
 
 interface IRequest {
 	user_id: string;
@@ -10,12 +11,19 @@ interface IRequest {
 export default class UserRentBookService {
 	constructor(
 		@inject('UsersRepository')
-		usersRepository: UsersRepository,
+		private usersRepository: UsersRepository,
 	) {}
 
 	public async execute({ user_id, book_id }: IRequest): Promise<any> {
 		console.log('UserRentBookService');
 		console.log(`User Id: [${user_id}] | Book Id: [${book_id}]`);
+
+		const userExists = await this.usersRepository.findById(user_id);
+
+		if (!userExists) {
+			// throw new AppError('User does not exists', 404)
+			console.log('[ERROR 404]: User does not exists');
+		}
 
 		return {
 			user_id,
