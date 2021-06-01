633f321552ee5198560ecba331c3c0e0abd8b2c6
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-01 16:56:41 -0300

    Verificação existencia stock

--- src/modules/libraries/services/UserRentBookService.ts
+++ src/modules/libraries/services/UserRentBookService.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 import UsersRepository from '@modules/users/repositories/UsersRepository';
 import AppError from '@shared/errors/AppError';
 import BooksRepository from '../repositories/BooksRepository';
+import StockLibraryRepository from '../repositories/StockLibraryRepository';
 
 interface IRequest {
 	user_id: string;
@@ -15,7 +16,9 @@ export default class UserRentBookService {
 		private usersRepository: UsersRepository,
 		@inject('BooksRepository')
 		private booksRepository: BooksRepository,
-	) {}
+	) // @inject('StockRepository')
+	// private stockRepository: StockLibraryRepository
+	{}
 
 	public async execute({ user_id, book_id }: IRequest): Promise<any> {
 		console.log('UserRentBookService');
@@ -29,12 +32,22 @@ export default class UserRentBookService {
 		if (!userExists) {
 			// throw new AppError('User does not exists', 404)
 			console.log('[ERROR 404]: User does not exists');
+			return;
 		}
 
 		if (!bookExists) {
 			// throw new AppError('Book does not exists', 404)
 			console.log('[ERROR 404]: Book does not exists');
+			return;
 		}
+
+		if (bookExists.stockLibrary.length === 0) {
+			// throw new AppError('Without book in stock', 400)
+			console.log('[ERROR 400]: Without book in stock');
+			return;
+		}
+
+		const stockItem = bookExists.stockLibrary[0];
 
 		return {
 			user_id,
