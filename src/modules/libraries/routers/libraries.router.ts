a184c553272e11738a1a783cf3c49248bfdb4305
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-02 02:57:28 -0300

    Remoção arquivo duplicado para alugar livro

--- src/modules/libraries/routers/libraries.router.ts
+++ src/modules/libraries/routers/libraries.router.ts
@@ -6,8 +6,6 @@ import SessionsLibraryController from '../controllers/SessionsLibraryController'
 import StockLibraryController from '../controllers/StockLibraryController';
 import imageUpload from '@shared/middlewares/imageUpload';
 
-import RentBookService from '../services/RentBookService';
-
 const librariesRouter = Router();
 
 const librariesController = new LibrariesController();
@@ -28,26 +26,7 @@ librariesRouter.post('/register-book', stockLibraryController.registerBook);
 
 librariesRouter.post('/sessions', sessionsLibraryController.create);
 
-// librariesRouter.post('/rent', async (request, response) => {
-// 	const { user_id, stock_library_id } = request.body;
-// 	const rentBookService = new RentBookService();
-
-// 	const bookRented = await rentBookService.execute({
-// 		user_id,
-// 		stock_library_id,
-// 	});
-
-// 	return response.json(bookRented);
-// });
-
 librariesRouter.post('/rent', rentBooksController.create);
-
-librariesRouter.get('/rent', async (request, response) => {
-	const rentBookService = new RentBookService();
-
-	const booksRented = await rentBookService.executeList();
-	return response.json(booksRented);
-});
 
 librariesRouter.get('/', librariesController.list);
 librariesRouter.get('/:slug', librariesController.show);
