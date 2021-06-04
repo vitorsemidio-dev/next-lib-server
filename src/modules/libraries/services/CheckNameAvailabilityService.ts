bd2cb3e9e8bb9398c0d4df0f2e64a7e26c9c6bbe
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-04 17:12:55 -0300

    Remoção código não utilizado no serviço

--- src/modules/libraries/services/CheckNameAvailabilityService.ts
+++ src/modules/libraries/services/CheckNameAvailabilityService.ts
@@ -1,17 +1,10 @@
-import { inject, injectable } from 'tsyringe';
-
 import AppError from '@shared/errors/AppError';
 import slugfy from '@utils/slugfy';
 
-import ILibrariesRepository from '../repositories/interfaces/ILibrariesRepository';
 import INameAvailabilityRepository from '../repositories/interfaces/INameAvailabilityRepository';
 
-// @injectable()
 export default class CheckNameAvailabilityService {
-	constructor(
-		// @inject('LibrariesRepository')
-		private entityRepository: INameAvailabilityRepository,
-	) {}
+	constructor(private entityRepository: INameAvailabilityRepository) {}
 
 	public async execute(name: string) {
 		const slug = slugfy(name);
