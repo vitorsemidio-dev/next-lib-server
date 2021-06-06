9547cfa6dd8229c35639a9de32b99335a5959eba
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-06 01:19:49 -0300

    Validações para atualização dados biblioteca

--- src/modules/libraries/services/UpdateLibraryService.ts
+++ src/modules/libraries/services/UpdateLibraryService.ts
@@ -1,11 +1,14 @@
 import { inject, injectable } from 'tsyringe';
 
+import Library from '@shared/database/entities/Library';
+import AppError from '@shared/errors/AppError';
 import HashProvider from '@utils/HashProvider';
+import slugfy from '@utils/slugfy';
 
 import LibrariesRepository from '../repositories/LibrariesRepository';
-import Library from '@shared/database/entities/Library';
 
 interface IRequest {
+	id: string;
 	name: string;
 	email: string;
 	password?: string;
@@ -20,11 +23,53 @@ export default class UpdateLibraryService {
 	) {}
 
 	public async execute({
+		id,
 		name,
 		email,
 		password,
 		avatar,
 	}: IRequest): Promise<Library> {
+		const library = await this.librariesRepository.findById(id);
+
+		if (!library) {
+			throw new AppError('Library does not found', 404);
+		}
+
+		if (email && email !== library.email) {
+			const isEmailAvailable =
+				await this.librariesRepository.checkEmailAvailability(email);
+
+			if (!isEmailAvailable) {
+				throw new AppError('E-mail is already used', 400);
+			}
+		}
+
+		if (name && name !== library.name) {
+			const isNameAvailable =
+				await this.librariesRepository.checkNameAvailability(name);
+
+			if (!isNameAvailable) {
+				throw new AppError('Nam eis already used', 400);
+			}
+		}
+
+		const slug = slugfy(name);
+
+		let hashedPassword = null;
+
+		if (password) {
+			hashedPassword = await HashProvider.generateHash(password);
+		}
+
+		const newDataLibrary = Object.assign(library, {
+			slug,
+			email,
+			avatar,
+			password: hashedPassword || library.password,
+		});
+
+		// await this.librariesRepository.update(newDataLibrary)
+
 		return {} as Library;
 	}
 }
