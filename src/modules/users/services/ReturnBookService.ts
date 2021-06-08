76661af427b85d9190df487784f30f67d5138870
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-08 17:19:37 -0300

    Identificado stock

--- src/modules/users/services/ReturnBookService.ts
+++ src/modules/users/services/ReturnBookService.ts
@@ -21,15 +21,30 @@ export default class ReturnBookService {
 		private stockLibraryRepository: IStockLibraryRepository,
 	) {}
 	public async execute({ user_id, book_id }: IRequest) {
-		const user = await this.usersRepository.findById(user_id);
+		// const user = await this.usersRepository.findById(user_id);
 
-		if (!user) {
-			throw new AppError('User does not found', 404);
-		}
+		// if (!user) {
+		// 	throw new AppError('User does not found', 404);
+		// }
 
+		// Lista Livros Alugados Pelo Usuario
 		const booksRented = await this.rentBookRepository.findByUserId(user_id);
 
-		const bookRented = booksRented.find((item) => item.book.id === book_id);
+		// const bookRented = booksRented.find((item) => item.book.id === book_id);
+
+		const stockIds = booksRented.map((item) => item.stock_library_id);
+
+		// Lista Stock
+		const stockWithBook = await this.stockLibraryRepository.findByIds(
+			stockIds,
+			['book'],
+		);
+
+		const stockWithExistentBooks = stockWithBook.filter((item) => item.book);
+
+		const stock = stockWithExistentBooks.find(
+			(item) => item.book_id === book_id,
+		);
 
 		// bookRented.status = 'Devolvido'
 
