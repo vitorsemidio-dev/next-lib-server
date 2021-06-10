61f19d729d7979003add574c265a87644f6e1a54
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-06-09 23:53:52 -0300

    RentBookDTO

---
+++ src/modules/libraries/dtos/IRentBookDTO.ts
@@ -0,0 +1,4 @@
+export default interface IRentBookDTO {
+	user_id: string;
+	stock_library_id: string;
+}
--- src/modules/libraries/repositories/interfaces/IRentBooksRepository.ts
+++ src/modules/libraries/repositories/interfaces/IRentBooksRepository.ts
@@ -1,9 +1,5 @@
+import IRentBookDTO from '@modules/libraries/dtos/IRentBookDTO';
 import RentBook from '@shared/database/entities/RentBook';
-
-interface IRentBookDTO {
-	user_id: string;
-	stock_library_id: string;
-}
 
 export default interface IRentBooksRepository {
 	createInstance(data: IRentBookDTO): RentBook;
